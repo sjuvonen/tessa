74f89d42181fd925db06494be251bb973ecc4a9f
Samu Juvonen <samu.juvonen@gmail.com>
2019-01-06 23:12:23 +0700

    Fix imports

--- tessa/__init__.py
+++ tessa/__init__.py
@@ -1,2 +1,3 @@
-from .main import init_profile, read_profile_meta, create_snapshot, read_snapshot_meta
+from .core import init_profile, read_profile_meta, read_snapshot_meta
+from .main import create_snapshot
 from .remote import send_snapshot_local, send_snapshot_ssh
